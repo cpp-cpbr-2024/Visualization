e9357bc674248bbf6d5f2fbe32017aaa502d1fab
Maciej Uberna <maciej.uberna@gmail.com>
2024-11-25 19:07:06 +0100

    Added MacOS paths to bitmap - but commented and PC uncommented.

--- src/main.cpp
+++ src/main.cpp
@@ -95,6 +95,8 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     //load textures here
     bg_texture_ = std::make_unique<Texture>(renderer, "../..//data//cs_bg.bmp", WINDOW_WIDTH, WINDOW_HEIGHT);
     plane_texture_ = std::make_shared<Texture>(renderer, "../..//data//plane1.bmp", 40, 40);
+    //bg_texture_ = std::make_unique<Texture>(renderer, "../../../../../data/cs_bg.bmp", WINDOW_WIDTH, WINDOW_HEIGHT);
+    //plane_texture_ = std::make_shared<Texture>(renderer, "../../../../../data/plane1.bmp", 40, 40);
 
 
     //==================//
